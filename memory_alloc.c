7e992940129a28b526f5ff2cfff98131b6c4ab7d
niketanand-7 <niketanand7@gmail.com>
2024-08-10 11:04:25 -0500

    linking memory blocks together

--- memory_alloc.c
+++ memory_alloc.c
@@ -2,26 +2,76 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+
+typedef char ALIGN[16];
 
 /*
 This makes it into a linked list of blocks of memory. Header + Block of Memory linked list.
 */
-struct header_t{
-    size_t size;
-    unsigned is_free;
-    struct header_t *next;
+union header {
+    struct {
+        size_t size;
+        unsigned is_free;
+        union header *next;
+    } s;
+    ALIGN stub;
 };
+typedef union header header_t;
+
+header_t *head, *tail;
+pthread_mutex_t global_malloc_lock;
+
+/*
+The sbrck(size) function is used to allocate memory to the block of size size.
+*/
+header_t *get_free_block(size_t size)
+{
+	header_t *curr = head;
+	while(curr) {
+		if (curr->s.is_free && curr->s.size >= size)
+			return curr;
+		curr = curr->s.next;
+	}
+	return NULL;
+}
 
 /*
 The malloc(size) function allocates size bytes to block and return a pointer to the memory as block.
 */
 void *malloc(size_t size){
+    size_t total_size;
     void *block;
     block = sbrck(size);
-
-    if (block == (void *)-1){
+    header_t *header;
+    if (!size){
         return NULL;
-    }else{
-        return block;
     }
+
+    pthread_mutex_lock(&global_malloc_lock);
+    header = get_free_block(size);
+
+    if (header){
+        header->s.is_free = 0;
+		pthread_mutex_unlock(&global_malloc_lock);
+		return (void*)(header + 1);
+    }
+    total_size = sizeof(header_t) + size;
+	block = sbrk(total_size);
+	if (block == (void*) -1) {
+		pthread_mutex_unlock(&global_malloc_lock);
+		return NULL;
+	}
+    header = block;
+	header->s.size = size;
+	header->s.is_free = 0;
+	header->s.next = NULL;
+	if (!head)
+		head = header;
+	if (tail)
+		tail->s.next = header;
+	tail = header;
+	pthread_mutex_unlock(&global_malloc_lock);
+	return (void*)(header + 1);
 }
+
