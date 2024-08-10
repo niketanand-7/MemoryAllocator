5f3dba0f2db3f2f780b53c71b7813500c91eede9
niketanand-7 <niketanand7@gmail.com>
2024-08-10 11:14:02 -0500

    calloc and realloc and free function

--- memory_alloc.c
+++ memory_alloc.c
@@ -37,6 +37,43 @@ header_t *get_free_block(size_t size)
 }
 
 /*
+The free(block) function is used to free the memory allocated to the block.
+ree() has to first deterimine if the block-to-be-freed is at the end of the heap.
+If it is, we can release it to the OS. Otherwise, all we do is mark it ‘free’, hoping to reuse it later.
+*/
+void free(void *block)
+{
+	header_t *header, *tmp;
+	void *programbreak;
+
+	if (!block)
+		return;
+	pthread_mutex_lock(&global_malloc_lock);
+	header = (header_t*)block - 1;
+
+	programbreak = sbrk(0);
+	if ((char*)block + header->s.size == programbreak) {
+		if (head == tail) {
+			head = tail = NULL;
+		} else {
+			tmp = head;
+			while (tmp) {
+				if(tmp->s.next == tail) {
+					tmp->s.next = NULL;
+					tail = tmp;
+				}
+				tmp = tmp->s.next;
+			}
+		}
+		sbrk(0 - sizeof(header_t) - header->s.size);
+		pthread_mutex_unlock(&global_malloc_lock);
+		return;
+	}
+	header->s.is_free = 1;
+	pthread_mutex_unlock(&global_malloc_lock);
+}
+
+/*
 The malloc(size) function allocates size bytes to block and return a pointer to the memory as block.
 */
 void *malloc(size_t size){
@@ -73,5 +110,47 @@ void *malloc(size_t size){
 	tail = header;
 	pthread_mutex_unlock(&global_malloc_lock);
 	return (void*)(header + 1);
+}
+
+/*
+The calloc(num, nsize) function allocates memory for an array of num elements of nsize bytes each and returns a pointer to the allocated memory.
+Additionally, the memory is all set to zeroes.
+*/
+void *calloc(size_t num, size_t nsize)
+{
+	size_t size;
+	void *block;
+	if (!num || !nsize)
+		return NULL;
+	size = num * nsize;
+	/* check mul overflow */
+	if (nsize != size / num)
+		return NULL;
+	block = malloc(size);
+	if (!block)
+		return NULL;
+	memset(block, 0, size);
+	return block;
+}
+
+/*
+The realloc(block, size) function changes the size of the memory block pointed to by block to size bytes.
+*/
+void *realloc(void *block, size_t size)
+{
+	header_t *header;
+	void *ret;
+	if (!block || !size)
+		return malloc(size);
+	header = (header_t*)block - 1;
+	if (header->s.size >= size)
+		return block;
+	ret = malloc(size);
+	if (ret) {
+		
+		memcpy(ret, block, header->s.size);
+		free(block);
+	}
+	return ret;
 }
 
